14a66771db9ac18bbd7c3dfaac6a585021077959
berbenzuel <zukovnik1010@gmail.com>
2026-02-09 19:24:12 +0100

    update script

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ let gridHeight = 20;
 
 const builderGrid = document.querySelector('#builder-grid');
 
-let data = [FieldType]
+let data = FieldType[]
 
 function generateGrid() {
     for (let i = 0; i < gridWidth*gridHeight; i++) {
