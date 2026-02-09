9573b98cdadb3efda07c5fe086dd6d0b74fc31c4
zukovnik <sdfsdf@dfs>
2026-02-09 09:40:38 +0100

    foo

---
+++ src/script.js
@@ -0,0 +1,13 @@
+﻿let gridWidth = 20;
+let gridHeight = 20;
+
+const builderGrid = document.querySelector('#builder-grid');
+
+let data = [FieldType]
+
+function generateGrid() {
+    for (let i = 0; i < gridWidth*gridHeight; i++) {
+
+    }
+
+}
